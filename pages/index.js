0d500466d18c69e53907e094d95fa1ece4ae049a
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-01 20:07:34 -0400

    222. Passing Through the Cookies

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log("printing currentUser");
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // we are on the server!
+    // requests should be made to DOMAIN/ROUTE i.e http://SERVICENAME.NAMESPACE.svc.cluster.local/ROUTE
+    const { data } = await axios.get(
+      "http://istio-ingressgateway.istio-system.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
