65045ef8f5c014a29516ba2974acb39ff7d9ec27
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-01 20:49:08 -0400

    Adds comments to the _app.js custom app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.css';
+// This component is used to add styles and components
+// that will be used/displayed in every page
+import "bootstrap/dist/css/bootstrap.css";
 
 export default ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
