c31fc94da0d1d5d23fdb3f1e29421e1e733af4a1
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-01 21:01:23 -0400

    227. Moving GetInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 // This component is used to add styles and components
 // that will be used/displayed in every page
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = () => {};
+
+export default AppComponent;
