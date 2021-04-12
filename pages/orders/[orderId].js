e7e57fe2cefcc2939dabc7982286bbc058cad423
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-12 03:02:26 -0400

    462. Configuring Stripe

--- pages/orders/[orderId].js
+++ pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(
@@ -28,7 +29,18 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      {/* look at nextjs docs for how to set this stripeKey as an env with kubernetes */}
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51If4DQGmbPFPG9W4yo5Hz6rcHkUBxMfo6de7EoKu0LaX2IZAZZh3DGQyjTN22K3rTAflHopprHYn4ZjCWqRNkSHK00alpwtLAA"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
